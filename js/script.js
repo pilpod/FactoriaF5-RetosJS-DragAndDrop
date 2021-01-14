81667cf0c4a159b8d4d120bf1d797c73f7454e38
pilpod <67429595+pilpod@users.noreply.github.com>
2021-01-14 23:31:10 +0100

    AddEventListener configured

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-const fill = document.querySelector('')
-const empties = document.querySelectorAll('')
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty')
 
-fill.addEventListener('dragstart', )
-fill.addEventListener('dragend', )
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
 
 for(const empty of empties) {
-
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
 }
 
 function dragStart() {
-
-    setTimeout()
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
 }
 
 function dragEnd() {
-    this.
+    this.className = 'fill';
 }
 
 function dragOver(e) {
-
+    e.preventDefault();
+    console.log('dragover');
 }
 
 function dragEnter(e) {
+    console.log('dragenter');
 
 }
 
-function dragLeave() {
+function dragLeave(e) {
+    console.log('dragleave');
 
 }
 
-function dragDrop() {
+function dragDrop(e) {
+    console.log('dragdrop');
 
 }
